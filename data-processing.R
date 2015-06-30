16f975e2a7220ccabbd278a1aaea62b5576481be
usr110 <m.aliabbas@gmail.com>
2015-06-30 17:23:49 +0100

    (Temporary) Removed baseline summary variables

--- data-processing.R
+++ data-processing.R
@@ -1,4 +1,6 @@
 tdata <- read.csv("data/reduced_tdata.csv")
 idata <- read.csv("data/idata.csv")
 sdata <- read.csv("data/aggr_summary.csv")
+# Temporarily remove baseline summary
+sdata <- sdata[-1,]
 sdata[is.na(sdata)] <- 0
