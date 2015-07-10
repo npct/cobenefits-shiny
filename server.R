07446f9fd23dad92d108d7289ead19b778ba70e0
usr110 <m.aliabbas@gmail.com>
2015-07-10 16:10:00 +0100

    Removed unused code

--- server.R
+++ server.R
@@ -44,57 +44,6 @@ shinyServer(function(input, output, session){
     pd <<- data
   })
   
-  #   output$plotMode <- renderPlot({
-  #     plotDataTable()
-  #     if (!is.null(pd)){
-  #       if (input$scenario == 't'){
-  #         filtered_title <- getFilteredTitle(tdata)
-  #         bcounts <- count(tdata, "MainMode_reduced_val")
-  #         bcounts$tp <- bcounts$freq / sum(bcounts$freq) * 100
-  #         bcounts$freq <- NULL
-  #         
-  #         scounts <- count(pd, "MainMode_reduced_val")
-  #         scounts$freq1 <- scounts$freq / sum(scounts$freq) * 100
-  #         
-  #         
-  #         
-  #         ecounts <- data.frame(v1=bcounts$tp, Filtered_Frequency=scounts[match(bcounts$MainMode_reduced_val, scounts$MainMode_reduced_val), 3])
-  #         
-  #         bcounts[["Total Population"]] <- bcounts$tp
-  #         bcounts$tp <- NULL
-  #         
-  #         bcounts[["Selected Population"]] <- ecounts$Filtered_Frequency
-  #         
-  #         
-  #         
-  #         df.long<-melt(bcounts)
-  #         
-  #         print(ggplot(df.long,aes(MainMode_reduced_val,value,fill=variable)) + ggtitle(paste("Main Mode: Total population versus population selected for scenario \n(selected population currently defined as ", filtered_title, ")", sep = "")) 
-  #               + geom_bar(stat="identity",position="dodge") 
-  #               + labs(x = "", y = "Frequency (%) ") + theme(text = element_text(size = 14)))
-  #         
-  #       }
-  #       
-  #       else{
-  #         filtered_title <- getFilteredTitle(idata)
-  #         max_val <- max(pd$total_mmet)
-  #         if (max_val < 60){
-  #           if (max_val > 5){
-  #             hist(pd$total_mmet, xlab = "Total Marginal MET", main = paste("Total Marginal MET of population selected for scenario \n(selected population currently defined as: ",filtered_title, ")", sep = ""),
-  #                  breaks = c(seq(min(pd$total_mmet), ceiling(max(pd$total_mmet)), by = 5), max(pd$total_mmet)))
-  #           }else{
-  #             hist(pd$total_mmet, xlab = "Total Marginal MET", main = paste("Total Marginal MET of population selected for scenario \n(selected population currently defined as: ",filtered_title, ")", sep = ""))
-  #           }
-  #         }
-  #         else{
-  #           hist(pd$total_mmet, xlab = "Total Marginal MET", main = paste("Total Marginal MET of population selected for scenario \n(selected population currently defined as: ",filtered_title, ")", sep = ""),
-  #                breaks = c(seq(min(pd$total_mmet), 60, by = 5),max(pd$total_mmet)), xlim = c(min(pd$total_mmet), 60), right=FALSE)
-  #         }
-  #       }
-  #     }
-  #     
-  #   })
-  
   output$plotMode <- renderChart({
     plotDataTable()
     if (!is.null(pd)){
@@ -124,8 +73,6 @@ shinyServer(function(input, output, session){
         scounts$freq1 <- scounts$freq / sum(scounts$freq) * 100
         scounts$freq1 <- round(scounts$freq1, digits = 1)
         
-        
-        
         ecounts <- data.frame(v1=bcounts$tp, Filtered_Frequency=scounts[match(bcounts$MainMode_reduced_val, scounts$MainMode_reduced_val), 3])
         
         bcounts[["Total Population"]] <- bcounts$tp
@@ -148,9 +95,6 @@ shinyServer(function(input, output, session){
       }else{
         filtered_title <- getFilteredTitle(idata)
         
-        
-        #h1$xAxis(categories = bc$Var1, title = list(text = 'Total Marginal MET'))
-        #h1$yAxis(title = list(text = 'Percentage %'))
         h1 <- Highcharts$new()
         h1$chart(type = "column")
         h1$plotOptions(column=list(animation=FALSE))
@@ -163,10 +107,7 @@ shinyServer(function(input, output, session){
         #cat("total: ", nrow(as.data.frame(bc)), "\n")
         bc1max <- max(bc$Freq, na.rm = T)
         
-        #         h1$xAxis(categories = bc$Var1, title = list(text = 'Total Marginal MET'))
         h1$xAxis(categories = as.list(append(as.numeric( sub("\\D*(\\d+).*", "\\1", bc$Var1[-1])), "60+")), title = list(text = 'Total Marginal MET'))
-        
-        
         h1$series(data = bc$Freq, name = "Total Population")
         #cat(nrow(pd), "\n")
         max_val <- 0
@@ -185,7 +126,6 @@ shinyServer(function(input, output, session){
         }
         else{
           #cat(c(seq(min(pd$total_mmet), 55, by = 5),max(pd$total_mmet)), "\n")
-          #bc <- table (cut (pd$total_mmet, breaks = c(seq(min(pd$total_mmet), 55, by = 5),max(pd$total_mmet)), xlim = c(min(pd$total_mmet), 60)))
           bc <- table (cut (pd$total_mmet, breaks = c(seq(min(pd$total_mmet), 60, by = 5),max(pd$total_mmet))))
         }
         extended_title <- paste("Total Marginal MET of population selected for scenario (selected population currently defined as: ",filtered_title, ")", sep = "")
@@ -221,8 +161,6 @@ shinyServer(function(input, output, session){
   output$plotBaseline <- renderChart({
     if (!is.null(tdata)){
       if (input$scenario == 'i'){
-        #         hist(idata$total_mmet, xlab = "Total Marginal MET", main = "Total Marginal MET of Total Population",
-        #              breaks = c(seq(min(idata$total_mmet), 60, by = 5),max(idata$total_mmet)), xlim = c(min(idata$total_mmet), 60), right=FALSE)
         
         filtered_title <- getFilteredTitle(idata)
         max_val <- max(idata$total_mmet)
@@ -236,9 +174,6 @@ shinyServer(function(input, output, session){
         h1$title(text = extended_title)
         h1$tooltip(valueSuffix= '%')
         h1$xAxis(categories = bc$Var1, title = list(text = 'Total Marginal MET'))
-        
-        
-        #h1$yAxis(title = list(text = 'Percentage %'))
         
         h1$chart(type = "column")
         h1$plotOptions(column=list(animation=FALSE))
@@ -310,8 +245,6 @@ shinyServer(function(input, output, session){
     data <- msharedtata
     data <- subset(data, MS == (as.numeric(lMS) + 1) & TDR == lTDR & equity == lEQ & ebike == lEB)
     
-    #cat(" test: ", dim(data), "\n")
-    
     data[is.na(data)] <- 0
     data <- arrange(data, MS)
     bd <<- data
@@ -326,8 +259,6 @@ shinyServer(function(input, output, session){
     lEQ <- input$inEQ
     
     data <- sdata
-    #     if (lMS != "All")
-    #       data <- subset(data, MS == lMS)# & TDR == lTDR & equity == lEQ & ebike == lEB)
     if (lTDR != "All")
       data <- subset(data, TDR == lTDR)
     if (lEQ != "All")
@@ -349,38 +280,15 @@ shinyServer(function(input, output, session){
     
   })
   
-  #   Solid
-  #   ShortDash
-  #   ShortDot
-  #   ShortDashDot
-  #   ShortDashDotDot
-  #   Dot
-  #   Dash
-  #   LongDash
-  #   DashDot
-  #   LongDashDot
-  #   LongDashDotDot
-  
   genericPlot <- function(var){
-    #     reactiveFunction()
-    #     cat("start", nrow(scdata), "\n")
-    #     if (!is.null(sdata)){}
-    #     cat(input$inTDR,  " : ", input$inEB, " : ", input$inEQ, "\n")
-    #if (input$inTDR == "All" & input$inMS == "All"){
-    #     input$inTDR
-    #     input$inEB
-    #     input$inEQ
     
     h1 <- Highcharts$new()
     h1$chart(type = "spline")
     # types of charts: http://api.highcharts.com/highcharts#plotOptions
     h1$yAxis(title = list(text = var))
-    #h1$xAxis(categories = sort(unique(sdata$MS), decreasing = F)[-1], title = list(text = 'Cycling Multiplier'))
     h1$xAxis(categories = sort(unique(sdata$MS), decreasing = F), title = list(text = 'Cycling Multiplier'))
-    #h1$legend(title = list(text = "(Click to disable/enable splines)"))
-    #h1$legend(symbolWidth = 80)
     
-    if (input$inTDR == "All"){                 # & input$inEB == "All" & input$inEQ == "All"){
+    if (input$inTDR == "All"){
       if (input$inEB != "All" & input$inEQ != "All"){
         sub1 <- subset(scdata, TDR == 0.7 & ebike == as.numeric(input$inEB) & equity == as.numeric(input$inEQ))
         h1$series(data = sub1[[var]], name = paste("TDR 0.7 (EB ", input$inEB, " and EQ ", input$inEQ, ")" , sep = ""))
@@ -511,12 +419,7 @@ shinyServer(function(input, output, session){
         h1$series(data = sub1[[var]], name = paste("TDR ", input$inTDR, " (EB 1 and EQ 1)"))
         
       }
-      
-      
-      
-      
     }
-    
     h1$exporting(enabled = T)
     return(h1)
   }
@@ -570,13 +473,6 @@ shinyServer(function(input, output, session){
       
       h1$tooltip(valueSuffix= '%')
       
-      #         walk
-      #         bike
-      #         car driver
-      #         car passenger
-      #         bus
-      #         train
-      #         other
       h1$set(dom = 'plotBDMode')
       h1$exporting(enabled = T)
       return (h1)
--- ui.R
+++ ui.R
@@ -6,11 +6,8 @@ if (!require(rCharts)) {
 }
 source("data-processing.R")
 uMS <- append("All", sort(unique(sdata$MS)))
-#uMS <- append("All", sort(unique(sdata$MS))[-1])
 uTDR <- append("All", sort(unique(sdata$TDR), decreasing = F))
-#uTDR <- append("All", sort(unique(sdata$TDR), decreasing = F)[-1])
 uEQ <- append("All",sort(unique(sdata$equity)))
-#uEQ <- sort(unique(sdata$equity))
 uEB <- append("All", sort(unique(sdata$ebike)))
 
 uBDMS <- (sort(unique(msharedtata$MS)[-1]) - 1)
@@ -18,7 +15,6 @@ uBDTDR <- sort(unique(msharedtata$TDR), decreasing = F)
 uBDEQ <- sort(unique(msharedtata$equity))
 uBDEB <- sort(unique(msharedtata$ebike))
 
-#uEB <- sort(unique(sdata$ebike))
 variableList <- t(as.matrix(colnames(sdata)))
 variableList <- variableList[,6:length(colnames(sdata))]
 
@@ -28,18 +24,13 @@ scenarios <- c("Trip Mode Share" = "t",
 ag <- "All"
 ag <- append(ag, sort(unique(as.character(tdata$age_group))))
 
-#ses <- append("All", sort(unique(as.character(tdata$NSSec_B03ID))))
-#ses <- c("All" = "All", "1 (richest)" = 1, "2" = 2, "3" = 3, "4" = 4, "5 (poorest)" = 5)
-
 ses <- c("All" = "All",
-          "Managerial and professional occupations" = 1,
-          "Intermediate occupations and small employers" = 2,
-          "Routine and manual occupations" = 3,
-          "Never worked and long-term unemployed" = 4,
-          "Not classified (including students)" = 5)
+         "Managerial and professional occupations" = 1,
+         "Intermediate occupations and small employers" = 2,
+         "Routine and manual occupations" = 3,
+         "Never worked and long-term unemployed" = 4,
+         "Not classified (including students)" = 5)
 
-
-#ethnicity <- append("All", sort(unique(as.character(tdata$EthGroupTS_B02ID))))
 ethnicity <- c("All" = "All", "White" = 1, "Non-white" = 2)
 
 gender <- c("All" = 3,
@@ -48,54 +39,47 @@ gender <- c("All" = 3,
 
 
 shinyUI(fluidPage(width="100%", height="100%",
-  headerPanel("Co-Benefit Model"),
-  sidebarPanel(
-    conditionalPanel(condition="input.conditionedPanels==1",
-                     #selectInput(inputId = "inMS", label = h4("Select Cycling Multiplier:"), choices =  uMS),
-                     selectInput(inputId = "inTDR", label = h4("Select Travel Distance Reduction (TDR):"), choices =  uTDR, selected = uTDR[length(uTDR)]),
-                     selectInput(inputId = "inEB", label = h4("Select Ebike (EB):"), choices =  uEB),
-                     selectInput(inputId = "inEQ", label = h4("Select Equity (EQ):"), choices =  uEQ),
-                     selectInput('varname', label = h4('Plot Variable:'), variableList)
-
-
-    ),
-    conditionalPanel(condition="input.conditionedPanels==2",
-                     selectInput(inputId = "inBDMS", label = h4("Select Cycling Multiplier:"), choices =  uBDMS),
-                     selectInput(inputId = "inBDTDR", label = h4("Select Travel Distance Reduction (TDR):"), choices =  uBDTDR, selected = uTDR[length(uTDR)]),
-                     selectInput(inputId = "inBDEB", label = h4("Select Ebike (EB):"), choices =  uBDEB),
-                     selectInput(inputId = "inBDEQ", label = h4("Select Equity (EQ):"), choices =  uBDEQ)
-    ),
-                     
-    conditionalPanel(condition="input.conditionedPanels==3",
-                     radioButtons("scenario", "Scenario:", scenarios, inline = TRUE),
-                     selectizeInput("ag", "Age Group:", ag, selected = ag[1], multiple = F), #options = list(maxOptions = 2)),
-                     radioButtons("gender", "Gender: ", gender, inline = TRUE),
-                     selectizeInput("ses", "Socio Economic Classification :", ses, selected = ses[1], multiple = F),
-                     selectizeInput("ethnicity", "Ethnic Group:", ethnicity, selected = ethnicity[1], multiple = F)
-    )
-  ),
-
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Scenarios", value=1,
-                showOutput('plotCycPercent', "highcharts"),
-                h4("(Click on the legend to enable/disable a line)", align="center"),
-                showOutput('plotGenericVariable', "highcharts"),
-                h4("(Click on the legend to enable/disable a line)", align="center"),
-                HTML('<style>iframe.rChart{ width: 100%; height: 400px;}</style>')
-               ),
-      tabPanel("Scenarios - Mode Share", value=2,
-               showOutput('plotBDMode', "highcharts")
-               ),
-    
-      tabPanel("Baseline", value=3,
-#                div(class='wrapper',
-#                    tags$style(".Nvd3{ height: 400px;}"),
-#                    showOutput("plotMode","Nvd3")
-#                ),
-                showOutput('plotMode', "highcharts"))
-#                 showOutput('plotBaseline', 'highcharts'))
-      , id = "conditionedPanels"
-    )
-  )
+                  headerPanel("Co-Benefit Model"),
+                  sidebarPanel(
+                    conditionalPanel(condition="input.conditionedPanels==1",
+                                     selectInput(inputId = "inTDR", label = h4("Select Travel Distance Reduction (TDR):"), choices =  uTDR, selected = uTDR[length(uTDR)]),
+                                     selectInput(inputId = "inEB", label = h4("Select Ebike (EB):"), choices =  uEB),
+                                     selectInput(inputId = "inEQ", label = h4("Select Equity (EQ):"), choices =  uEQ),
+                                     selectInput('varname', label = h4('Plot Variable:'), variableList)
+                    ),
+                    
+                    conditionalPanel(condition="input.conditionedPanels==2",
+                                     selectInput(inputId = "inBDMS", label = h4("Select Cycling Multiplier:"), choices =  uBDMS),
+                                     selectInput(inputId = "inBDTDR", label = h4("Select Travel Distance Reduction (TDR):"), choices =  uBDTDR, selected = uTDR[length(uTDR)]),
+                                     selectInput(inputId = "inBDEB", label = h4("Select Ebike (EB):"), choices =  uBDEB),
+                                     selectInput(inputId = "inBDEQ", label = h4("Select Equity (EQ):"), choices =  uBDEQ)
+                    ),
+                    
+                    conditionalPanel(condition="input.conditionedPanels==3",
+                                     radioButtons("scenario", "Scenario:", scenarios, inline = TRUE),
+                                     selectizeInput("ag", "Age Group:", ag, selected = ag[1], multiple = F), #options = list(maxOptions = 2)),
+                                     radioButtons("gender", "Gender: ", gender, inline = TRUE),
+                                     selectizeInput("ses", "Socio Economic Classification :", ses, selected = ses[1], multiple = F),
+                                     selectizeInput("ethnicity", "Ethnic Group:", ethnicity, selected = ethnicity[1], multiple = F)
+                    )
+                  ),
+                  
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel("Scenarios", value=1,
+                               showOutput('plotCycPercent', "highcharts"),
+                               h4("(Click on the legend to enable/disable a line)", align="center"),
+                               showOutput('plotGenericVariable', "highcharts"),
+                               h4("(Click on the legend to enable/disable a line)", align="center"),
+                               HTML('<style>iframe.rChart{ width: 100%; height: 400px;}</style>')
+                      ),
+                      tabPanel("Scenarios - Mode Share", value=2,
+                               showOutput('plotBDMode', "highcharts")
+                      ),
+                      
+                      tabPanel("Baseline", value=3,
+                               showOutput('plotMode', "highcharts"))
+                      , id = "conditionedPanels"
+                    )
+                  )
 ))
