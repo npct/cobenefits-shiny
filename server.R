d225b7b008b2e47b26b4f39afd74ad9a5a2e5103
usr110 <m.aliabbas@gmail.com>
2015-07-13 16:04:23 +0100

    Introduced active and super active population

--- server.R
+++ server.R
@@ -152,8 +152,9 @@ shinyServer(function(input, output, session){
         
         if(filter){
           h1$series(data = bc$Freq, name = "Selected Population")
-          mmet_per <- round(nrow(subset(pd, total_mmet >= 8.75 & total_mmet <= 17.5)) / nrow(pd) * 100, digits = 1)
-          h1$subtitle(text = paste("Selected population with marginal MET between 8.75 and 17.5: ", mmet_per, "%", sep=""), style = list(font = 'bold 12px "Trebuchet MS", Verdana, sans-serif'))
+          mmet_per_8.75 <- round(nrow(subset(pd, total_mmet >= 8.75 )) / nrow(pd) * 100, digits = 1)
+          mmet_per_17.5 <- round(nrow(subset(pd, total_mmet <= 17.5)) / nrow(pd) * 100, digits = 1)
+          h1$subtitle(text = paste("Selected population with marginal MET >= 8.75: ", mmet_per_8.75, "%", " and >= 17.5: ", mmet_per_17.5, "%", sep=""), style = list(font = 'bold 12px "Trebuchet MS", Verdana, sans-serif'))
         }else{
           h1$subtitle(text = HTML("Sorry: Not Enough Data to Display Selected Population (Population Size &lt; 10)"), style = list(font = 'bold 14px "Trebuchet MS", Verdana, sans-serif', color = "#f00"))
         }
